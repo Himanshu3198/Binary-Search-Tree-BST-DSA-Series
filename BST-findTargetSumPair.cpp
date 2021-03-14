8e9b04976251ec9bcdd74603bdb160e2bfa0c562
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-03-15 00:13:47 +0530

    Add files via upload

---
+++ BST-findTargetSumPair.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        unordered_set<int>s;
+    bool solve(TreeNode*root,int &k){
+       
+         if(root==NULL){
+             return false;
+         }
+        
+        if(s.find(k- root->val)!=s.end()){
+            return true;
+        }
+        else{
+            s.insert(root->val);
+        }
+        return solve(root->left,k) || solve(root->right,k);
+        
+   
+        
+    }
+    bool findTarget(TreeNode* root, int k) {
+        
+          
+        if(solve(root,k)){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+        
+    }
+};
