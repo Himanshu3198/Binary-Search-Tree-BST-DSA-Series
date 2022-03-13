ce0ae4e5fc3ad742ee3d8005f63375027c69783e
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-13 13:55:14 +0530

    Update BST-bst-to-greater-sum-tree.cpp

--- BST-bst-to-greater-sum-tree.cpp
+++ BST-bst-to-greater-sum-tree.cpp
@@ -1,48 +1,18 @@
-// 1038. Binary Search Tree to Greater Sum Tree
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    
-      void solve(TreeNode *root,stack<TreeNode*>&st){
-          
-          if(!root) return ;
-          
-          solve(root->left,st);
-           st.push(root);
-          solve(root->right,st);
-      }
-    TreeNode* bstToGst(TreeNode* root) {
-        stack<TreeNode*>st;
+        int prevTotal=0;
+    void transformTree(struct Node *root)
+    {
+        //code here
+         // we will use property of reverse inorder because inorder gives a sorted list in asceding
+        //   order but if we do reverse inorder like visit right child first then it will be in 
+        // descending order
         
-        solve(root,st);
-        
-         TreeNode *t=st.top();
-        st.pop();
-    int prev=t->val;
-        
-        while(!st.empty()){
-            
-            TreeNode *temp=st.top();
-            st.pop();
-            
-            temp->val=temp->val+prev;
-            
-            prev=temp->val;
-            
-            
-        }
-        
-        return root;
+        if(!root) return ;
+         transformTree(root->right);
+         int temp=root->data;
+         
+         root->data=prevTotal;
+         prevTotal+=temp;
+         
+         transformTree(root->left);
+         
     }
-};
